a91cfa337317037f7e257497f454b5a5dce8e49b
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-12-06 16:12:56 -0500

    trying to fix push for switching to login component...

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import {
   Redirect,
   Route,
   Switch,
+  useHistory
 } from 'react-router-dom';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -27,7 +28,7 @@ function App() {
   const dispatch = useDispatch();
 
   const user = useSelector(store => store.user);
-  
+  const history = useHistory()
   const [startingScreenClicked, setStartingScreenClicked] = useState(false);
 
   useEffect(() => {
@@ -36,99 +37,45 @@ function App() {
 
   const handleStartingScreenClick = () => {
     setStartingScreenClicked(true);
+    history.push('/home');
   };
 
   if (!startingScreenClicked) {
-    return <ClickToStart onClick={handleStartingScreenClick} />;}
+    return <ClickToStart onClick={handleStartingScreenClick} />;
+  }
+
     
   return (
     <Router>
       <div>
         <Nav />
         <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
           <Redirect exact from="/" to="/home" />
-
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
-
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
-          </ProtectedRoute>
-
-          <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the login page
-              <LoginPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the registration page
-              <RegisterPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the Landing page
-              <LandingPage />
-            }
-          </Route>
-
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
-            <h1>404</h1>
-          </Route>
+          <Route exact path="/about" component={AboutPage} />
+          <ProtectedRoute exact path="/user" component={UserPage} />
+          <ProtectedRoute exact path="/info" component={InfoPage} />
+          <Route 
+            exact 
+            path="/login" 
+            render={() => user.id ? <Redirect to="/user" /> : <LoginPage />}
+          />
+          <Route 
+            exact 
+            path="/registration" 
+            render={() => user.id ? <Redirect to="/user" /> : <RegisterPage />}
+          />
+          <Route 
+            exact 
+            path="/home" 
+            render={() => user.id ? <Redirect to="/user" /> : <LandingPage />}
+          />
+          <Route render={() => <h1>404</h1>} />
         </Switch>
         <Footer />
       </div>
     </Router>
   );
+  
 }
 
 export default App;
