b08d9ed4cb4c8487ed3f3e091b68b27db45b7bb5
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2024-02-05 11:50:38 -0500

    added more info on main page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -24,92 +24,58 @@ function UserPage() {
                 </ul>
             </nav>
             <section id="about-me">
-                <h1>About Me:</h1>
-            </section>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+        <h2>About Me</h2>
+        <p>
+            I'm a dedicated Full-Stack Developer with a rich background in technical and maintenance roles, 
+            currently based in [Your Location]. Transitioning from hands-on roles at the City of Saint Petersburg 
+            and USA Construction Group, where I developed a keen sense of problem-solving, leadership, and technical 
+            proficiency, I've recently completed an immersive coding program at Prime Digital Academy. 
+            This program equipped me with expertise in JavaScript, React, Node.js, and other modern web technologies, 
+            fueling my passion for building responsive, user-centric applications.
+        </p>
+        <p>
+             My project portfolio includes a vehicle maintenance tracking app and a mental health-focused mobile app,
+             demonstrating my capability in full-stack development, client collaboration, and project management. 
+             With a toolkit spanning across JavaScript, SQL, HTML, CSS, TypeScript, and various development tools, 
+             I'm eager to bring my growth mindset, adaptability, and creativity to the tech industry. 
+             My aim is to leverage my diverse skills to develop innovative software solutions that make a difference.
+        </p>
+    </section>
+            
             <section id="contact">
                 <h1>Contact:</h1>
+                <p>Phone Number</p>
+                <p>Email</p>
+                <p>linkedin URL</p>
             </section>
             <br/>
             <br/>
             <br/>
             <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+        
             <br/>
             <section id="education">
-                <h1>Education:
-                </h1>
+              <h1>Education:</h1>
+                <ul>
+                  <li>
+                    <strong>PRIME DIGITAL ACADEMY: Full-Stack Developer Student</strong> - Minneapolis, MN (Remote)
+                    <p>(January 2023 - July 2023)</p>
+                    <p>An coding immersion program that focuses on relevant technologies and methods within the full-stack software development field. Technologies include JavaScript, HTML 5, CSS, React, jQuery, PostgreSQL, and Node.js.</p>
+                  </li>
+                </ul>
             </section>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            
+            <br />
+
+
             <section id="skills">
                 <h1>Skills:</h1>
+                <p>
+                <strong>Technical Skills:</strong> Programs - Postico, Postman, VS Code, MacOS, Windows, Slack, GitHub, DB Designer, VS Live-share, Zoom, Trello, Google Suite, Office Suite, Outlook, ERD Design, Wireframing. Languages - JavaScript, TypeScript, Jest, AWS, HTML, CSS, jQuery, JSX/React, Redux, Saga, SQL, Express, NodeJS, git, API’s, Sweet Alert, Chart.js, .NET Framework, Branches and Pull Requests. Worked with some Python and C# language.
+            </p>
+            <p>
+                <strong>Soft Skills:</strong> Growth mindset, adaptable, creative, lifelong learner, curious, problem solver, resourceful, work well on a team, critical thinker.
+            </p>
             </section>
             <br/>
             <br/>
@@ -118,26 +84,18 @@ function UserPage() {
             <br/>
             <br/>
             <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+        
             <section id="experience">
                 <h1>Work Experience:</h1>
+            <div>
+                <h2>PRIME DIGITAL ACADEMY: Full-Stack Developer Student</h2>
+                <p>Minneapolis, MN (Remote) | January 2023 - July 2023</p>
+                <ul>
+                    <li>An intensive coding immersion program focusing on full-stack software development with technologies such as JavaScript, HTML 5, CSS, React, jQuery, PostgreSQL, and Node.js.</li>
+                    <li><strong>Solo Project:</strong> Developed a full-stack application for managing vehicle maintenance. Utilized technologies like Javascript, HTML, CSS, SweetAlert2, Express, and more.</li>
+                    <li><strong>Client Project:</strong> Contributed as a full-stack developer on a team project for a mental health and financial decision-making mobile app. Handled both backend and frontend development using various technologies.</li>
+                </ul>
+            </div>
             </section>
             <br/>
             <br/>
